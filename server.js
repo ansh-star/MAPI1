795833c7914150d434215edee4c065c29997e99c
ansh-star <96314656+ansh-star@users.noreply.github.com>
2024-10-27 14:13:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
 });
 
 // Starting the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
