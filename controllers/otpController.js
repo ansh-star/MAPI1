22d2426b960ae4dae4f89c6112bed7d4cdc5085d
Arpan Nema <96337337+arpannema1234@users.noreply.github.com>
2024-10-28 12:05:04 +0530

    Update otpController.js

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const Admin = require("../models/Admin");
 const { generateToken } = require("../utils/tokenGenerator"); // Assume token generation function is available
 const User = require("../models/User");
+const Roles = require("../utils/roles");
 
 // Function to send OTP
 const sendOTP = async (req, res) => {
@@ -46,15 +47,47 @@ const verifyOTP = async (req, res) => {
     if (response.data.Status !== "Success") {
       return res.status(200).json({ success: false, message: "Invalid OTP" });
     } else {
-      if (adminkey) {
-        const admin = await Admin.findOne({ mobileNumber: phone, adminKey });
+      if (adminKey) {
+        const admin = await Admin.findOne({ mobileNumber: phone, adminKey })
+          .populate({
+            path: "wholesalerRequests", // Path to populate
+            match: { role: Roles.WHOLESALER, user_verified: false }, // Condition: Only fetch users with role: 1 (wholesalers) and user_verified: false
+          })
+          .populate({ path: "productList", option: { sample: 50 } });
         if (!admin) {
           return res
             .status(403)
             .json({ success: false, message: "Invalid Admin credentials" });
+        } else {
+          return res.status(200).json({
+            success: true,
+            message: "Valid OTP",
+            user: admin.toObject(),
+            token: generateToken({ _id: admin._id, role: Roles.ADMIN }),
+          });
         }
       }
-      return res.status.json({ success: true, message: "Valid OTP" });
+
+      const user = await User.findOne({ mobileNumber: phone })
+        .populate({ path: "products", option: { limit: 50 } })
+        .populate({
+          path: "cart.productId", // Populates the 'productId' inside 'cart'
+          options: { limit: 50 },
+        });
+
+      if (!user) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid mobile number.",
+        });
+      }
+
+      return res.status.json({
+        success: true,
+        message: "Valid OTP",
+        user: user.toObject(),
+        token: generateToken(user),
+      });
     }
   } catch (error) {
     res.status(500).json({
