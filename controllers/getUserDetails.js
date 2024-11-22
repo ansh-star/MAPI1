691a8833b338a6f0930c192e6d93ee5476051f00
Arpan <arpannema1234@gmail.com>
2024-11-22 22:53:03 +0530

    update get cart logic

--- controllers/getUserDetails.js
+++ controllers/getUserDetails.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const User = require("../models/User");
 
 const userDetails = async (req, res) => {
@@ -12,7 +13,7 @@ const userDetails = async (req, res) => {
 
     res.status(200).json({ success: true, user });
   } catch (error) {
-    res.status(500).json({
+    res.status(200).json({
       success: false,
       message: "Error occurred while fetching user details",
     });
@@ -26,14 +27,67 @@ const getCart = async (req, res) => {
     const user = await User.findById(id)
       .select("cart")
       .populate("cart.productId");
-
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    if (user.cart?.length === 0) {
+      return res
+        .status(200)
+        .json({ success: false, message: "Cart is empty", cart: [] });
+    }
     return res.status(200).json({ success: true, cart: user.cart });
   } catch (error) {
     console.error(error);
-    res.status(500).json({
+    res.status(200).json({
       success: false,
       message: "Error occurred while fetching cart details",
     });
   }
 };
-module.exports = { userDetails, getCart };
+
+const getWholesalerRequest = async (req, res) => {
+  const { id } = req.user;
+  const { limit, pageNumber } = req.query;
+  try {
+    const user = await User.aggregate([
+      { $match: { _id: id } }, // Match the user by ID
+      {
+        $project: {
+          wholesalerRequests: {
+            $slice: [
+              "$wholesalerRequests",
+              (parseInt(pageNumber) - 1) * 10,
+              parseInt(limit),
+            ],
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "users", // Reference the User collection
+          localField: "wholesalerRequests",
+          foreignField: "_id",
+          as: "wholesalerRequests",
+        },
+      },
+    ]);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, wholesalerRequests: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occurred while fetching wholesaler requests",
+    });
+  }
+};
+
+module.exports = { userDetails, getCart, getWholesalerRequest };
